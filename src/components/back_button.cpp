10e0446ee556fc69c8cae298ee5c2d43c987cd34
RomanGalynov <144688026+Roman-Galynov-InVeritaSoft@users.noreply.github.com>
2026-03-04 10:24:04 +0200

    Added minor UI fix to back button

--- src/components/back_button.cpp
+++ src/components/back_button.cpp
@@ -14,14 +14,15 @@ lv_obj_t * create_back_button(lv_obj_t * parent, lv_obj_t * target_screen)
   /* We remove all the standard style */
   lv_obj_remove_style_all(btn);
 
-  lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 10, 10);
+  lv_obj_align(btn, LV_ALIGN_TOP_LEFT, 10, 0);
 
   /* We make the size according to the content */
-  lv_obj_set_size(btn, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+  lv_obj_set_size(btn, LV_SIZE_CONTENT, 40);
 
   lv_obj_t * label = lv_label_create(btn);
   lv_label_set_text(label, LV_SYMBOL_LEFT " Go Back");
   lv_obj_set_style_text_color(label, lv_color_white(), 0);
+  lv_obj_center(label);
 
   lv_obj_add_event_cb(btn, back_event_cb, LV_EVENT_CLICKED, target_screen);
 
