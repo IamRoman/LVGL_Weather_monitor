a468e962249e23cef60e490778a957e4268a7e4b
RomanGalynov <144688026+Roman-Galynov-InVeritaSoft@users.noreply.github.com>
2026-03-05 12:44:30 +0200

    Added kayboard Animation to Settings screen

--- src/screens/screen_location.cpp
+++ src/screens/screen_location.cpp
@@ -7,12 +7,18 @@
 #include <config_service.h>
 
 static lv_obj_t *screen;
+static lv_obj_t *anim_cont;
 static lv_obj_t *city_container;
 static lv_obj_t *ta_lat;
 static lv_obj_t *ta_lon;
 
 static lv_obj_t *selected_btn = NULL;
 static lv_obj_t *selected_location;
+
+static lv_obj_t *kb;
+
+static lv_style_t style_ta_main;
+static lv_style_t style_ta_cursor;
 
 static void city_select(lv_obj_t *btn)
 {
@@ -43,6 +49,51 @@ static void city_event_cb(lv_event_t *e)
   lv_textarea_set_text(ta_lon, buf);
 }
 
+static void anim_y_cb(void *var, int32_t v)
+{
+  lv_obj_set_y((lv_obj_t *)var, v);
+};
+
+static void anim_screen(bool isMovingUp)
+{
+  lv_anim_t anim;
+  lv_anim_init(&anim);
+  lv_anim_set_var(&anim, anim_cont);
+  if (isMovingUp)
+  {
+    lv_anim_set_duration(&anim, 100);
+    lv_anim_set_values(&anim, lv_obj_get_y(anim_cont), -100);
+    lv_anim_set_path_cb(&anim, lv_anim_path_overshoot);
+  }
+  else
+  {
+    lv_anim_set_duration(&anim, 100);
+    lv_anim_set_values(&anim, lv_obj_get_y(anim_cont), 0);
+    lv_anim_set_path_cb(&anim, lv_anim_path_ease_in);
+  }
+  lv_anim_set_exec_cb(&anim, anim_y_cb);
+  lv_anim_start(&anim);
+};
+
+static void ta_event_cb(lv_event_t *e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+  lv_obj_t *ta = (lv_obj_t *)lv_event_get_target(e);
+  lv_obj_t *kb = (lv_obj_t *)lv_event_get_user_data(e);
+  if (code == LV_EVENT_FOCUSED)
+  {
+    lv_keyboard_set_textarea(kb, ta);
+    lv_obj_remove_flag(kb, LV_OBJ_FLAG_HIDDEN);
+    anim_screen(true);
+  }
+  else if (code == LV_EVENT_DEFOCUSED)
+  {
+    lv_keyboard_set_textarea(kb, NULL);
+    lv_obj_add_flag(kb, LV_OBJ_FLAG_HIDDEN);
+    anim_screen(false);
+  }
+};
+
 static void confirm_event_cb(lv_event_t *e)
 {
   float lat = atof(lv_textarea_get_text(ta_lat));
@@ -53,12 +104,38 @@ static void confirm_event_cb(lv_event_t *e)
   char buf_long[40];
   sprintf(buf_long, "Selected location: %.4f, %.4f", lat, lon);
   lv_label_set_text(selected_location, buf_long);
+
+  anim_screen(false);
+}
+
+static void keyboard_event_cb(lv_event_t *e)
+{
+  lv_event_code_t code = lv_event_get_code(e);
+
+  if (code == LV_EVENT_READY)
+  {
+    float lat = atof(lv_textarea_get_text(ta_lat));
+    float lon = atof(lv_textarea_get_text(ta_lon));
+
+    config_set_location(lat, lon);
+
+    char buf_long[40];
+    sprintf(buf_long, "Selected location: %.4f, %.4f", lat, lon);
+    lv_label_set_text(selected_location, buf_long);
+
+    anim_screen(false);
+  }
 }
 
 void screen_location_init(void)
 {
 	screen = lv_obj_create(NULL);
 	lv_obj_set_style_bg_color(screen, COLOR_BG_DARK, 0);
+
+  anim_cont = lv_obj_create(screen);
+  lv_obj_set_size(anim_cont, LV_PCT(100), LV_PCT(100));
+  lv_obj_set_style_bg_opa(anim_cont, LV_OPA_TRANSP, LV_PART_MAIN);
+  lv_obj_set_style_border_opa(anim_cont, LV_OPA_TRANSP, LV_PART_MAIN);
 
   /*Title*/
   static lv_style_t style_H1_label;
@@ -71,14 +148,21 @@ void screen_location_init(void)
   lv_style_set_text_color(&style_secondary_label, WHITE);
   lv_style_set_text_font(&style_secondary_label, &lv_font_montserrat_14);
 
+  /* ===== Keyboard ===== */
+  kb = lv_keyboard_create(screen);
+  lv_keyboard_set_mode(kb, LV_KEYBOARD_MODE_NUMBER); // only numbers, a dot and a minus
+  lv_obj_set_size(kb, LV_PCT(100), 150);
+  lv_obj_add_flag(kb, LV_OBJ_FLAG_HIDDEN); // hidden at first
+  lv_obj_add_event_cb(kb, keyboard_event_cb, LV_EVENT_ALL, NULL);
+
   /* Title */
-  lv_obj_t *title = lv_label_create(screen);
+  lv_obj_t *title = lv_label_create(anim_cont);
   lv_label_set_text(title, "Location settings");
   lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 10);
   lv_obj_add_style(title, &style_H1_label, 0);
 
   /* Selected location */
-  selected_location = lv_label_create(screen);
+  selected_location = lv_label_create(anim_cont);
   lv_label_set_text(selected_location, "Selected location: --,--");
   lv_obj_align(selected_location, LV_ALIGN_TOP_MID, 0, 35);
   lv_obj_add_style(selected_location, &style_secondary_label, 0);
@@ -87,7 +171,7 @@ void screen_location_init(void)
   create_back_button(screen, screen_settings_get());
 
   /*--- Horizontal list ---*/
-  city_container = lv_obj_create(screen);
+  city_container = lv_obj_create(anim_cont);
   lv_obj_set_width(city_container, LV_PCT(100));
   lv_obj_set_height(city_container, 60);
   lv_obj_align(city_container, LV_ALIGN_TOP_MID, 0, 60);
@@ -127,32 +211,68 @@ void screen_location_init(void)
     }
   }
 
+  /* ===== Textarea MAIN style ===== */
+  lv_style_init(&style_ta_main);
+  lv_style_set_text_color(&style_ta_main, WHITE);
+  lv_style_set_bg_opa(&style_ta_main, LV_OPA_TRANSP);
+
+  /* ===== Cursor style ===== */
+  lv_style_init(&style_ta_cursor);
+  lv_style_set_bg_color(&style_ta_cursor, LIGHT_BLUE);
+  lv_style_set_bg_opa(&style_ta_cursor, LV_OPA_COVER);
+  lv_style_set_width(&style_ta_cursor, 1);
+
   /* ===== Latitude ===== */
 
-  ta_lat = lv_textarea_create(screen);
+  ta_lat = lv_textarea_create(anim_cont);
   lv_obj_set_size(ta_lat, 200, 40);
   lv_obj_align(ta_lat, LV_ALIGN_TOP_MID, 0, 150);
   lv_textarea_set_placeholder_text(ta_lat, "Latitude");
-  lv_obj_set_style_text_color(ta_lat, WHITE, 0);
-  lv_obj_set_style_bg_opa(ta_lat, LV_OPA_TRANSP, LV_PART_MAIN);
+
+  lv_obj_add_style(ta_lat, &style_ta_main, LV_PART_MAIN);
+  lv_obj_add_style(ta_lat, &style_ta_cursor, LV_PART_CURSOR);
+
+  /*Open keyboard*/
+  lv_obj_add_event_cb(ta_lat, ta_event_cb, LV_EVENT_ALL, kb);
+  lv_keyboard_set_textarea(kb, ta_lat);
 
   // /* ===== Longitude ===== */
 
-  ta_lon = lv_textarea_create(screen);
+  ta_lon = lv_textarea_create(anim_cont);
   lv_obj_set_size(ta_lon, 200, 40);
   lv_obj_align(ta_lon, LV_ALIGN_TOP_MID, 0, 200);
   lv_textarea_set_placeholder_text(ta_lon, "Longitude");
-  lv_obj_set_style_text_color(ta_lon, WHITE, 0);
-  lv_obj_set_style_bg_opa(ta_lon, LV_OPA_TRANSP, LV_PART_MAIN);
+
+  lv_obj_add_style(ta_lon, &style_ta_main, LV_PART_MAIN);
+  lv_obj_add_style(ta_lon, &style_ta_cursor, LV_PART_CURSOR);
+
+  /*Open keyboard*/
+  lv_obj_add_event_cb(ta_lon, ta_event_cb, LV_EVENT_ALL, kb);
+  lv_keyboard_set_textarea(kb, ta_lon);
 
   /* ===== Confirm ===== */
-
   lv_obj_t *btn_confirm = lv_button_create(screen);
-  lv_obj_align(btn_confirm, LV_ALIGN_BOTTOM_MID, 0, -20);
+  lv_obj_remove_style_all(btn_confirm);
+  lv_obj_set_style_bg_opa(btn_confirm, LV_OPA_TRANSP, 0);
+  lv_obj_set_style_border_opa(btn_confirm, LV_OPA_TRANSP, 0);
 
-  lv_obj_t *lbl = lv_label_create(btn_confirm);
-  lv_label_set_text(lbl, "Confirm");
-  lv_obj_center(lbl);
+  lv_obj_set_size(btn_confirm, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
+
+  lv_obj_align(btn_confirm, LV_ALIGN_TOP_RIGHT, -10, 10);
+
+  // Making a horizontal layout
+  lv_obj_set_flex_flow(btn_confirm, LV_FLEX_FLOW_ROW);
+  lv_obj_set_style_pad_gap(btn_confirm, 5, 0);
+
+  // Btn icon
+  lv_obj_t *icon = lv_label_create(btn_confirm);
+  lv_label_set_text(icon, LV_SYMBOL_OK);
+  lv_obj_set_style_text_color(icon, WHITE, 0);
+
+  // Btn text
+  lv_obj_t *label = lv_label_create(btn_confirm);
+  lv_label_set_text(label, "Confirm");
+  lv_obj_set_style_text_color(label, WHITE, 0);
 
   lv_obj_add_event_cb(btn_confirm, confirm_event_cb, LV_EVENT_CLICKED, NULL);
 
